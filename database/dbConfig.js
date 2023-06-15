c520abee7a6a73ecc93075493ce255c3ac483f59
Md. Adnan Akhtar <adnanakhtar1710@gmail.com>
2023-06-15 11:37:14 +0530

    modified error message in console

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -18,10 +18,7 @@ const dbConfig = {
                 console.log('Database connected successfully');
             })
             .catch((error) => {
-                // console.error('Database connection error:', error);
-                errorStr = 'Please specify a valid database connection string'
-                console.error(errorStr);
-                return errorStr;
+                console.error('Please specify a valid database connection string. Connection error :', error);
             });
     }
 };
